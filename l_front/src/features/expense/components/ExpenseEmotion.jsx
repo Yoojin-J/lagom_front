9935b9b369b47bfad61649b58e60daa274dd6f5d
Yoojin-J <syoojin03@gmail.com>
2026-05-11 01:15:11 +0900

    expense update
    부정적 감정 + 낮은 만족도일 때 뜨는 alert banner 제작

--- l_front/src/features/expense/components/ExpenseEmotion.jsx
+++ l_front/src/features/expense/components/ExpenseEmotion.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ChevronRight from '../../../assets/icons/common/ChevronRight';
 
 const ExpenseEmotion = ({
   formData,
@@ -10,8 +11,8 @@ const ExpenseEmotion = ({
     // 이미 선택된 걸 다시 누르면 null로 초기화(해제), 아니면 새로운 값 세팅
     // setSelectedEmo((prev) => (prev === val ? null : val));
     setFormData((prev) => ({
-      ...prev, 
-      emotion: prev.emotion === val ? null : val 
+      ...prev,
+      emotion: prev.emotion === val ? null : val
     }));
   };
 
@@ -20,8 +21,15 @@ const ExpenseEmotion = ({
     // setSelectedSat((prev) => (prev === val ? null : val));
     setFormData((prev) => ({
       ...prev, //
-      evaluation: prev.evaluation === val ? null : val 
+      evaluation: prev.evaluation === val ? null : val
     }));
+  };
+
+  const goHappyBank = () => {
+    // 행복해지는 저금으로 가기전 일단 가계부 저장 (fetch, post)
+    // 그 후에 행복저금으로 이동
+    // 행복저금으로 이동할 때 금액만 들고 가면 되는건가? (navigate)
+
   };
 
   return (
@@ -57,6 +65,15 @@ const ExpenseEmotion = ({
           })}
         </ul>
       </div>
+      { (formData.emotion === 3 || formData.emotion === 4 || formData.emotion === 5) && (formData.evaluation === 0 || formData.evaluation === 25) &&
+        <div className='alert-banner'>
+          <div className='alert-text'>
+            <div className='text1'>현재 소비 감정, 만족도가 낮아요</div>
+            <div className='text2'>행복 저금으로 기분을 전환해볼까요?</div>
+          </div>
+          <ChevronRight stroke="#F7645F" />
+        </div>
+      }
     </div>
   )
 }
